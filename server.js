65ff08742c7ea371522ae2fbeffc423c8a5a28ee
jessny <jessnyjoseph@gmail.com>
2021-01-26 21:03:31 -0800

    syntax errors and formatting complete

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.get("/notes", function (req, res) {
 
 // api notes
 app.get('/api/notes', (req, res) => {
-    console.log("it works");
     const data = fs.readFileSync("db/db.json", { encoding: "utf8" });
-    console.log("it is reading");
     var newData = JSON.parse(data);
     return res.json(newData);
 });
@@ -37,14 +35,14 @@ app.post("/api/notes", (req, res) => {
     notes.push(newNote);
     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
-        const database = JSON.parse(data);
-        for (var i = 0; i < database.length; i++) {
-                const noteCard = {
-                    title: database[i].title,
-                    text: database[i].text,
-                    id: i+1
-                };
-                notes.push(noteCard);
+        var newData = JSON.parse(data);
+        for (var i = 0; i < newData.length; i++) {
+            const noteCard = {
+                title: newData[i].title,
+                text: newData[i].text,
+                id: i + 1,
+            };
+            notes.push(noteCard);
         }
         fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes, null, 10), (err) => {
             if (err) throw err;
@@ -53,21 +51,19 @@ app.post("/api/notes", (req, res) => {
     });
 })
 
-
-// api/delete/note
+// api delete
 app.delete("/api/notes/:id", (req, res) => {
     const notes = [];
     var noteId = parseInt(req.params.id);
     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
-        const database = JSON.parse(data);
-        // const notes = [];
-        for (var i = 0; i < database.length; i++) {
-            if (i !== noteId) {
+        var newData = JSON.parse(data);
+        for (var i = 0; i < newData.length; i++) {
+            if (i != noteId) {
                 const noteCard = {
-                    title: database[i].title,
-                    text: database[i].text,
-                    id: notes.length
+                    title: newData[i].title,
+                    text: newData[i].text,
+                    id: notes.length,
                 };
                 notes.push(noteCard);
             }
@@ -79,22 +75,6 @@ app.delete("/api/notes/:id", (req, res) => {
     });
 })
 
-
-//         var dataDel = JSON.parse(data);
-//         dataDel.push(noteId);
-//         dataDel = dataDel.filter(function(note) {
-//             return note.id != noteId
-//         });
-//         fs.writeFile("/db/db.json", JSON.stringify(dataDel), function (err){
-//             if (err) throw err;
-//             console.log("Success")
-//         });
-
-//     });
-//     res.sendFile(path.join(__dirname, "db/db.json"))
-// });
-
-
 // index.html
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -105,23 +85,3 @@ app.listen(PORT, () => {
     console.log("App listening on PORT http://localhost:" + PORT)
 })
 
-
-// post api / notes
-// app.post('/api/notes', (req, res) => {
-//     var newNote = req.body;
-//     console.log("api notes");
-//     console.log(newNote);
-//     notes.push(newNote);
-//     fs.readFile("db/db.json", (err, data) => {
-//         // console.log(data);
-//         if (err) throw err;
-//         var newData = JSON.parse(data);
-//         newData.push(newNote);
-//         console.log(newData);
-//         // console.log(data.title);
-
-//         fs.writeFile("db/db.json", JSON.stringify(newData), (err) => {
-//             if (err) throw err;
-//         });
-//     });
-// });
